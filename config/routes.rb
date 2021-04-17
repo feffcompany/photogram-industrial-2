9ee6c093784add6b321374be01792e193f15ee24
demostudent14 <demostudent14@firstdraft.com>
2021-04-17 22:01:04 +0000

    Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root "photos#index"
 
-  
   devise_for :users
   
   resources :comments
   resources :follow_requests
   resources :likes
   resources :photos
-  resources :users, only: :show
+
+  get "/:username" => "users#show"
 end
