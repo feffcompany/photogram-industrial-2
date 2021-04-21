25fc776d36257f93ba96d12f0e97f26f99886bfa
jhoffmeyer <jhoffmeyer@chicagobooth.edu>
2021-04-21 20:38:28 +0000

    Better validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :likes
-  has_many :comments
-  has_many :photos
-  has_many :following, :through => :follow_requests
-  has_many :followers, :through => :follow_requests    
+  has_many :likes, foreign_key: :fan_id
+  has_many :comments, foreign_key: :author_id
+  has_many :own_photos, foreign_key: :owner_id, class_name: "Photo"
+  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"   
 end
